e4349a09b7ec6c1e775e82b6d67186f87494e318
Sky Gwak <hannelgwak@gmail.com>
2022-02-24 00:06:38 -0800

    file change

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/studentProfile4714'),
+      dir: require('path').join(__dirname, './coverage/golinksassessment'),
       subdir: '.',
       reporters: [
         { type: 'html' },
